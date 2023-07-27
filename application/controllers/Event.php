c7e594b43ae19595c4119f467afb79fb1aa56276
Scwarzcopv <galihadifirmansah88@gmail.com>
2023-07-27 15:19:58 +0700

    View Hasil Rapat

---
+++ application/controllers/Event.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Event extends CI_Controller
+{
+    private $data;
+    public function __construct()
+    {
+        parent::__construct();
+        is_log_in();
+        $this->load->model('Sidebar_model', 'sidebar');
+        // Get data 'user'
+        $this->data = array(
+            "user" => $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array(),
+        );
+        // Get session 'role_id'
+        $this->role = $this->session->userdata('role_id');
+    }
+    public function index()
+    {
+        // Session
+        $data['user'] = $this->data['user'];
+        // Title
+        $data['title'] = 'Hasil Rapat';
+        // Active Sidebar
+        $data['sidebar'] = 'Hasil Rapat';
+        // Judul Sidebar
+        $data['role'] = $this->sidebar->sidebar($this->role);
+
+        // Load View
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('event/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/views/event/index.php
@@ -0,0 +1,70 @@
+<main class="content">
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12">
+                <div class="card box shadow-rapat rounded-4" data-id="1">
+                    <div class="p-3">
+                        <table>
+                            <tr>
+                                <td>
+                                    <img src="<?= base_url('assets'); ?>/img/icons/envelope2.png" alt="Christina Mason" class="img-fluid" width="100" height="100" />
+                                </td>
+                                <td>
+                                    <table class="ms-3">
+                                        <tr>
+                                            <td class="fw-bold m-0 p-0 card-title">KADIN OPD UJI COBA 1</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="h3 fs-2 fw-bold m-0 p-0 "><a href="">Cek Kesehatan</a></td>
+                                        </tr>
+                                        <tr>
+                                            <td> 005/535/000.000/2023</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="row">
+
+            <div class="col-12 col-md-6 col-lg-3" id="card_forum" name="card_forum">
+                <div class="card box" data-id="1">
+                    <div class="card-header px-4 pt-4">
+                        <h5 class="card-title mb-0">Cek Kesehatan</h5>
+                        <div class="badge my-2 bg-info text-wrap"> KADIN OPD UJI COBA 1</div>
+                    </div>
+                    <div class="card-body px-4 pt-2">
+                        <table class="text-break">
+                            <tr>
+                                <td class="pe-2"><i class="fa-solid fa-calendar-check"></i></td>
+                                <td>Kamis, 20 Juli 2023</td>
+                            </tr>
+                            <tr>
+                                <td><i class="fa-solid fa-envelope-open-text"></i></td>
+                                <td>005/535/000.000/2023</td>
+                            </tr>
+                            <tr>
+                                <td><i class="fa-solid fa-location-dot"></i></td>
+                                <td>Kediriiiiiiiiiiiiiiiiii</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div> -->
+    </div>
+</main>
+
+
+
+
+<!-- Tutup elemen dari topbar.php -->
+</div>
+<?= $this->session->flashdata('message'); ?>
+<script>
+</script>
